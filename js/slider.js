d5823da94835b7b818263557e1296702012a5245
Nazar0vIvan <vano888888@gmail.com>
2023-04-10 00:28:56 +0300

    refactor: add some js refactoring

--- js/slider.js
+++ js/slider.js
@@ -1,20 +1,22 @@
-images = [
-  'sld-buddy-guy.jpg',
-  'sld-elton.jpg',
-  'sld-hendrix.jpg',
-  'sld-mozart.jpg',
-  'sld-shaw.jpg',
-  'sld-tommy1.jpg',
-  'sld-waits.jpg',
-  'sld-adams.jpg'
-]
 
-function set_random_image() {
-  console.log(1)
-  random_index = Math.floor(Math.random() * images.length);
-  selected_image = images[random_index]
+function set_random_images() {
 
-  console.log(selected_image)
+  const pb = [
+    { path: "sld-buddy-guy.jpg", background: '#d3c7ba' },
+    { path: "sld-elton.jpg",     background: '#efe3d7' },
+    { path: "sld-hendrix.jpg",   background: '#f6cda7' },
+    { path: "sld-mozart.jpg",    background: '#f5a46f' },
+    { path: "sld-shaw.jpg",      background: '#c5c5c5' },
+    { path: "sld-tommy1.jpg",    background: '#d4b194' },
+    { path: "sld-waits.jpg",     background: '#c1c1c1' },
+    { path: "sld-adams.jpg",     background: '#d1f1ff' }
+  ];
+  
+  (() => {
+    random_index = Math.floor(Math.random() * pb.length);
+    selected_pb = pb[random_index]
 
-  document.getElementById('slider').style.backgroundImage = `url('/static/images/${selected_image}')`
+    document.getElementById('slider').style.backgroundImage = `url('/static/images/${selected_pb.path}')`
+    document.getElementById('welcome-section').style.backgroundColor = selected_pb.background
+  })();
 }
